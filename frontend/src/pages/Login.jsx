ac99791fcaade67a5e23f9a58bcfe239b40c03e4
Omshi255 <omshisen795@gmail.com>
2025-05-03 01:31:29 +0530

    render

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -161,7 +161,7 @@ const Login = () => {
     e.preventDefault();
     setLoading(true); // 👈 Start loader
     try {
-      const response = await axios.post('http://localhost:5000/api/auth/login', {
+      const response = await axios.post('https://instructifyfin.onrender.com/api/auth/login', {
         email,
         password,
       });
