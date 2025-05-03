93cd1319eb0344211a7fa7c9b7f1049f552662a6
Omshi255 <omshisen795@gmail.com>
2025-05-03 13:08:18 +0530

    server.js code changed and react scripts downloaded

--- backend/server.js
+++ backend/server.js
@@ -15,14 +15,15 @@ import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const app = express();
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+
+// Serve static files from React
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../frontend', 'build', 'index.html'));
   });
 }
-
 
 dotenv.config();
 
